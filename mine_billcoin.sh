bc2e4c227242199c1b04a6cc8f07242222ea03ad
hashgrind <hashgrind@dc614.org>
2021-03-06 15:49:50 -0800

    Update mining address

--- mine_billcoin.sh
+++ mine_billcoin.sh
@@ -2,5 +2,5 @@
 echo "Mining... Press [CTRL+C] to stop"
 while :
 do
-  ./src/billcoin-cli generatetoaddress 1 BCms75uKFPPxc1jDc4KXCbTRH9rmCVKZmF
+  ./src/billcoin-cli generatetoaddress 1 bXt6vFUCNEoGXSjCykfwmfnyvR6xw57djw
 done
